a2afab4419e229cf1e461c9b28908944784656ea
Dipanshu Roy <roydipanshu02@gmail.com>
2026-03-10 09:46:11 +0530

    Arrow func,object,Array

---
+++ program.js
@@ -0,0 +1,44 @@
+//Function return
+const res=mul(12,30)
+function mul(x,y)
+{
+    return(x*y);
+}
+
+//Arrow Function
+const add2=(a,b)=>
+{
+    console.log(a+b);
+};
+add2(102,20);
+
+//Arrow Function for single line parameters
+const add3=(c,d)=>console.log(c+d);
+add3(30,20);
+
+//Object Creation
+
+
+//Array
+
+var myinfo=new Array ();
+var myinfo1=Array();
+console.log(myinfo,myinfo1);
+
+//Program
+let collection=
+[
+  {},
+  [],
+  true,
+  "john",
+  function(){},
+  9009898193,
+  undefined,
+  null,
+  //new string("abc"),
+  new Date(),
+];
+console.log(collection);
+collection.teacherName="JohnMac";
+collection.phoneNo=950008193;
